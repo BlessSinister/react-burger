29429f8a20ce7ea415ed0495e5fac1bcb2fd918a
Bless <ig.marks@yandex.ru>
2023-08-23 11:37:49 +0300

    complete_tabs_elem

--- src/components/tabs/tabs.jsx
+++ src/components/tabs/tabs.jsx
@@ -1,33 +1,22 @@
 import React from 'react'
-import Tab from '../tab/tab'
-import tabs from './tabs.module.css'
-export default function Tabs() {
-  const [current, setCurrent] = React.useState('one')
 
+import tabs from './tabs.module.css'
+import { Tab } from '@ya.praktikum/react-developer-burger-ui-components'
+const Tabs = () => {
+  const [current, setCurrent] = React.useState('one')
   return (
     <div style={{ display: 'flex' }} className={`mb-10 ${tabs.tabs_wrapper}`}>
-      <Tab
-        value="one"
-        active={current === 'one'}
-        onClick={setCurrent}
-        name="Булки"
-      ></Tab>
-      <Tab
-        value="two"
-        active={current === 'two'}
-        onClick={setCurrent}
-        name="Соусы"
-      >
-        Two
+      <Tab value="one" active={current === 'one'} onClick={setCurrent}>
+        Булки
       </Tab>
-      <Tab
-        value="three"
-        active={current === 'three'}
-        onClick={setCurrent}
-        name="Новинки"
-      >
-        Three
+      <Tab value="two" active={current === 'two'} onClick={setCurrent}>
+        Соусы
+      </Tab>
+      <Tab value="three" active={current === 'three'} onClick={setCurrent}>
+        Новинки
       </Tab>
     </div>
   )
 }
+
+export default Tabs
