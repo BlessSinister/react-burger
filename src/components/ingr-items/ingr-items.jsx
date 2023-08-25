9599af24d2a3eb1d7a6bcded67be355d6b8dc427
Bless <ig.marks@yandex.ru>
2023-08-25 20:12:27 +0300

    ref_font_family_price

--- src/components/ingr-items/ingr-items.jsx
+++ src/components/ingr-items/ingr-items.jsx
@@ -5,11 +5,15 @@ export default function IngrItems(props) {
     <div className={`${ing_items_style.card} mr-6 mb-8`}>
       <img src={props.image} alt="" />
       <div className={`${ing_items_style.price_wrapper} mt-2 mb-2`}>
-        <p className={`mr-2 text text_type_main-medium`}>{props.price}</p>
+        <p
+          className={`mr-2 text text_type_main-medium ${ing_items_style.text_decor}`}
+        >
+          {props.price}
+        </p>
         <CurrencyIcon type="primary" />
       </div>
 
-      <p className={`text text_type_main-small`}>{props.name}</p>
+      <p className={`text text_type_main-small `}>{props.name}</p>
     </div>
   )
 }
