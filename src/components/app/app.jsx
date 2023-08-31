a76e64fe62de653402027f40b61ec51f7e8fc217
Bless <ig.marks@yandex.ru>
2023-08-31 19:52:32 +0300

    ref_getData_fn

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -3,23 +3,18 @@ import AppHeader from '../app-header/app-header'
 import BurgerIngredients from '../burger-ingredients/burger-ingredients'
 import BurgerConstructor from '../burger-constructor/burger-constructor'
 import { useEffect, useState } from 'react'
-import { checkResponse, url } from '../../utils/api'
+import { getData } from '../../utils/api'
 import Modal from '../modal/modal'
 import IngredientDetails from '../ingredient-details/ingredient-details'
 import OrderDetails from '../order-detailse/order-details'
 import { Context } from '../hooks/context'
+
 export default function App() {
   const [state, setState] = useState({})
   const [orderNumber, setOrderNumber] = useState('034546')
 
   useEffect(() => {
-    try {
-      fetch(url)
-        .then((res) => checkResponse(res))
-        .then((data) => setState(data))
-    } catch (e) {
-      console.log(e)
-    }
+    getData().then((data) => setState(data))
   }, [])
 
   return (
--- src/utils/api.js
+++ src/utils/api.js
@@ -1,7 +1,15 @@
 const api = 'https://norma.nomoreparties.space/api/'
 const url = `${api}ingredients`
-const checkResponse = (res) => {
-  return res.ok ? res.json() : res.json().then((err) => Promise.reject(err))
+async function getData() {
+  try {
+    const response = await fetch(url)
+    if (response.ok) {
+      const data = await response.json()
+      return data
+    }
+  } catch (err) {
+    console.log(err)
+  }
 }
 
-export { checkResponse, url }
+export { getData }
