844f64b03d35f541c756469758118ef94bb552be
Bless <ig.marks@yandex.ru>
2023-11-09 16:36:11 +0300

    deleted_action_middle

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -2,7 +2,7 @@ import { DndProvider } from 'react-dnd'
 import { HTML5Backend } from 'react-dnd-html5-backend'
 import BurgerIngredients from '../../components/burger-ingredients/burger-ingredients'
 import BurgerConstructor from '../../components/burger-constructor/burger-constructor'
-import { useEffect } from 'react'
+
 import { modalFlag, modalOrderFlag } from '../../services/reducer'
 import { useAppDispatch } from '../../services/redux-hooks'
 
